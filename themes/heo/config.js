6b513c2e11144dcdccda5b0d328f8ef6235b58c5
yiyi1217-mc <135409834+yiyi1217-mc@users.noreply.github.com>
2023-12-08 20:30:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,8 +14,8 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 一个神奇的博客',
+  HEO_HERO_TITLE_LINK: 'https://boke2.yiyi1234.com/article/d33d2277-442e-4275-998c-c515bc9762d1',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
